8b5ff5d74c73577d29cfc34221bb516efb98e628
Daniel Chiu <daniechiu@gmail.com>
2017-04-27 22:24:47 -0400

    Convert task name display to EditableText

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ITask } from "../model/ITask";
-import { Checkbox } from "@blueprintjs/core";
+import { Checkbox, EditableText } from "@blueprintjs/core";
 
 export interface TaskProps {
 	task: ITask;
@@ -14,9 +14,9 @@ export class Task extends React.Component<TaskProps, {}> {
 					checked={this.props.task.completed}
 				/>
 				<span className={"pt-icon-standard pt-icon-star" + (this.props.task.important ? "" : "-empty") }/>
-				<span>
-					{this.props.task.name}
-				</span>
+				<EditableText
+					value={this.props.task.name}
+				/>
 			</li>
 		)
 	}

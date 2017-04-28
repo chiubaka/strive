a0d63b0a43ab71a3dd51e16c4e0771dd534d5a40
Daniel Chiu <daniechiu@gmail.com>
2017-04-27 23:47:26 -0700

    Get webpack-dev-server hot reloading working again
    * Generate index.html
    * Create a dev-only config which doesn't use ExtractTextPlugin

---
+++ webpack.config.dev.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "serenity.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      { test: /\.html$/, loader: "html-loader" },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      // Compile all '.scss' files through 'sass-loader'.
+      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+      { test: /\.(woff|woff2)$/, loader: "url-loader?name=fonts/[hash].[ext]&limit=5000&mimetype=application/font-woff" },
+      { test: /\.(eot|svg|ttf)$/, loader: "file-loader?name=fonts/[hash].[ext]" }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Serenity"
+    })
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,9 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const config = require("./webpack.config.dev.js");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "serenity.js",
-    path: __dirname + "/dist"
-  },
+config.plugins.push(new ExtractTextPlugin({
+  filename: "serenity.css",
+  allChunks: true
+}));
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-      // Compile all '.scss' files through 'sass-loader'.
-      { test: /\.scss$/, loaders: ExtractTextPlugin.extract("css-loader!sass-loader") },
-      { test: /\.(woff|woff2)$/, loader: "url-loader?name=fonts/[hash].[ext]&limit=5000&mimetype=application/font-woff" },
-      { test: /\.(eot|svg|ttf)$/, loader: "file-loader?name=fonts/[hash].[ext]" }
-    ],
-  },
-
-  plugins: [
-    new ExtractTextPlugin({
-      filename: "serenity.css",
-      allChunks: true
-    })
-  ]
-};
+module.exports = config;

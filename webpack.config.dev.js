7ecac4bc94d839e718bb3d335184c2d56843a781
Daniel Chiu <daniechiu@gmail.com>
2017-04-27 23:57:14 -0700

    Add react-hot-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,7 +18,7 @@ module.exports = {
     rules: [
       { test: /\.html$/, loader: "html-loader" },
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.tsx?$/, loaders: ["react-hot-loader/webpack", "awesome-typescript-loader"] },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       // Compile all '.scss' files through 'sass-loader'.

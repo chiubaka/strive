9c99bdb629ca5ca57d62207a168e673bfe1c98be
Daniel Chiu <daniechiu@gmail.com>
2017-05-19 07:38:10 +0200

    Swap out for generic Response and Request handlers. Support multiple request formats in API

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
   url(r"^tasks/$", views.task_list),
   url(r"^tasks/(?P<pk>[0-9]+)/$", views.task_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/api/views.py
+++ server/api/views.py
@@ -1,45 +1,44 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from api.models import Task
 from api.serializers import TaskSerializer
 
-@csrf_exempt
-def task_list(request):
+@api_view(["GET", "POST"])
+def task_list(request, format=None):
   if request.method == "GET":
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
 
   elif request.method == "POST":
-    data = JSONParser().parse(request)
-    serializer = TaskSerializer(data=data)
+    serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def task_detail(request, pk):
+@api_view(["GET", "PUT", "DELETE"])
+def task_detail(request, pk, format=None):
   try:
     task = Task.objects.get(pk=pk)
   except Task.DoesNotExist:
-    return HttpResponse(status=404)
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
   if request.method == "GET":
     serializer = TaskSerializer(task)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
   elif request.method == "PUT":
-    data = JSONParser().parse(request)
-    serializer = TaskSerializer(task, data=data)
+    serializer = TaskSerializer(task, data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
   elif request.method == "DELETE":
     task.delete()
-    return HttpResponse(status=204)
+    return Response(status=status.HTTP_204_NO_CONTENT)
